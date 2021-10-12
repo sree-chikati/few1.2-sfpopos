f79b429ff5016756bf0b1a80d7670b878315071c
Sreenvitha Chikati <srechi453.fusdk12@gmail.com>
2021-10-12 05:35:50 -0700

    header component built

---
+++ src/Title.js
@@ -0,0 +1,16 @@
+// src/Title.js
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div className="Title">
+        <header>
+            <h1>SFPOPOS</h1>
+            <div className="Title-Subtitle">San Francisco Privately Owned Public Open Spaces</div>
+        </header>
+    </div>
+    )
+}
+
+export default Title
