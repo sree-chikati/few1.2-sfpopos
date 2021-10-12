c4603fa899d23d5f04a90ceef73a567e4cf40e08
Sreenvitha Chikati <srechi453.fusdk12@gmail.com>
2021-10-12 06:03:27 -0700

    dynamic content and footer

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -3,17 +3,13 @@ import logo from './logo.svg'
 import './POPOSSpace.css';
 
 function POPOSSpace(props) {
-    const { name, image, address } = props
+    const { name, image, address, hours } = props
     return (
-      <div>
-        <img
-          src={`${process.env.PUBLIC_URL}images/${image}`}
-          width="300"
-          height="300"
-          alt="Hello"
-        />
+      <div className="POPOSSpace">
         <h1>{name}</h1>
+        <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
         <div>{address}</div>
+        <div>{hours}</div>
       </div>
     )
 }
