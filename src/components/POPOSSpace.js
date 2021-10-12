5c73e9b543f3494c5473797b1e085444cebcc7b8
Sreenvitha Chikati <srechi453.fusdk12@gmail.com>
2021-10-12 09:59:06 -0700

    Styling Spaces

--- src/components/POPOSSpace.js
+++ src/components/POPOSSpace.js
@@ -6,14 +6,17 @@ function POPOSSpace(props) {
     const { name, image, address, hours, id } = props
     return (
       <div className="POPOSSpace">
-          <Link to={`/details/${id}`}>
-            {name}
+        <Link className="POPOSSpace-title" to={`/details/${id}`}>
+          <h1>{name}</h1>
         </Link>
         <Link to={`/details/${id}`}>
             <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
         </Link>
-        <div>{address}</div>
-        <div>{hours}</div>
+        <div className="POPOSSpace-info">
+          <div >{address}</div>
+          <div>{hours}</div>
+			  </div>
+
       </div>
     )
 }
